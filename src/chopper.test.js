d3cbf4be1ccb661e08eeb629fa9acd8574ba4446
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-06-02 12:53:00 -0700

    fix windows testing

--- src/chopper.test.js
+++ src/chopper.test.js
@@ -1,40 +1,38 @@
 // @flow
 /* global test, expect, describe, afterEach */
 
-import path from 'path'
 import fs from 'fs-extra'
+import tmp from 'tmp'
 
 import Chopper from './chopper'
 
 const LOG_CHOPPER_MAX = 100000
-const tempErrorLog = path.join(__dirname, '..', 'tmp', 'error.log')
 
 function createTmpErrorLog (logLength = LOG_CHOPPER_MAX) {
+  let f = tmp.tmpNameSync()
   let createBits = []
   for (var i = 0; i < (logLength - 1); i++) createBits.push('a\n')
   createBits.push('a')
-  fs.ensureFileSync(tempErrorLog)
-  fs.writeFileSync(tempErrorLog, createBits.join(''))
+  fs.outputFileSync(f, createBits.join(''))
+  return f
 }
 
-function errorLogLength () {
-  return fs.readFileSync(tempErrorLog).toString().split('\n').length
+function errorLogLength (f: string) {
+  return fs.readFileSync(f).toString().split('\n').length
 }
-
-afterEach(() => {
-  fs.removeSync(tempErrorLog)
-})
 
 describe('Chopper.chop()', () => {
   test('truncates to 100 lines', async () => {
-    createTmpErrorLog()
-    await Chopper.chop(tempErrorLog, 100)
-    expect(errorLogLength()).toBe(100)
+    let f = createTmpErrorLog()
+    await Chopper.chop(f, 100)
+    expect(errorLogLength(f)).toBe(100)
+    fs.removeSync(f)
   })
 
   test('does not truncate when max is greater', async () => {
-    createTmpErrorLog()
-    await Chopper.chop(tempErrorLog, LOG_CHOPPER_MAX * 2)
-    expect(errorLogLength()).toBe(LOG_CHOPPER_MAX)
+    let f = createTmpErrorLog()
+    await Chopper.chop(f, LOG_CHOPPER_MAX * 2)
+    expect(errorLogLength(f)).toBe(LOG_CHOPPER_MAX)
+    fs.removeSync(f)
   })
 })
